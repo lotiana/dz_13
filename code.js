a8dc72c29b87c1cb6121d14543ae718bca22c189
Paul <isakura313@gmail.com>
2019-12-02 21:31:05 +0300

    first commit

---
+++ code.js
@@ -0,0 +1,22 @@
+let slider = $(".slider");
+let sliderItems = slider.find(".slider-item");
+
+// MVC
+//Model
+let showIndex = 0;
+
+
+
+slider.find('.slider-arrow-left').click(function(){
+
+    if( showIndex <= 0){
+        sliderItems.eq(showIndex).fadeOut();
+        sliderItems[showIndex]
+        showIndex = sliderItems.length - 1;
+        sliderItems.eq(showIndex).css('display', 'flex');
+    } else{
+        sliderItems.eq(showIndex).fadeOut();
+        sliderItems.eq(showIndex-1).css('display', 'flex');
+        showIndex--;
+    }
+})
