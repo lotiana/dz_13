d74f806473399f95c1eeaf1098f2fbe51e657ebd
Lotiana <Leight_star@mail.ru>
2019-12-03 22:52:26 +0300

    slider right

--- code.js
+++ code.js
@@ -20,3 +20,17 @@ slider.find('.slider-arrow-left').click(function(){
         showIndex--;
     }
 })
+
+slider.find('.slider-arrow-right').click(function(){
+
+    if( showIndex >= sliderItems.length){
+        sliderItems.eq(showIndex).fadeOut();
+        sliderItems[showIndex]
+        showIndex = sliderItems.length + 1;
+        sliderItems.eq(showIndex).css('display', 'flex');
+    } else{
+        sliderItems.eq(showIndex).fadeOut();
+        sliderItems.eq(showIndex+1).css('display', 'flex');
+        showIndex++;
+    }
+})
